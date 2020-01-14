ebbf76b3ec58e2bce8fb0e9fcb4384f191eff557
DanielGodfrey-Dev <DanielGodfrey-Dev@outlook.com>
2020-01-13 22:58:46 -0800

    Updated for Discover and Maestro

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,21 +1,32 @@
-// Given a credit card number, this function should return a string with the 
-// name of a network, like 'MasterCard' or 'American Express'
-// Example: detectNetwork('343456789012345') should return 'American Express'
+/*Note: `cardNumber` will always be a string
 
-// How can you tell one card network from another? Easy! 
-// There are two indicators:
-//   1. The first few numbers (called the prefix)
-//   2. The number of digits in the number (called the length)
+The Diner's Club network always starts with a 38 or 39 and is 14 digits long
+
+The American Express network always starts with a 34 or 37 and is 15 digits long
+
+Visa always has a prefix of 4 and a length of 13, 16, or 19.
+
+MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
+
+Discover always has a prefix of 6011, 644-649, or 65, and a length of 16 or 19.
+
+Maestro always has a prefix of 5018, 5020, 5038, or 6304, and a length of 12-19.*/
 
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
 
 var prefix = cardNumber.slice(0, 2); //first two nums of CC
+var threePrefix = cardNumber.slice(0, 3); //first three nums of CC
+var fourPrefix = cardNumber.slice(0, 4); //first four nums of CC
 var result; //CC network to be identified
+
+//for checking Maestro CC# length
+function lengthCheck() {
+	for (let i = 12; i <= 19; i++) {
+		if (cardNumber.length = i) {
+			return true;
+		    }
+      }
+  }
 
 //checking Diner's Club
 if ((prefix === '38' || prefix === '39') && cardNumber.length == 14) {
@@ -35,6 +46,17 @@ else if (prefix[0] === '4' && (cardNumber.length === 13 || cardNumber.length ===
 //checking MasterCard
 else if ((prefix === '51' || prefix === '52' || prefix === '53' || prefix === '54' || prefix === '55') && cardNumber.length === 16) {
   result = 'MasterCard';
+}
+
+//checking Discover
+else if ((fourPrefix === '6011' || threePrefix === '644' || threePrefix === '645' || threePrefix === '646' || threePrefix === '647' 
+	|| threePrefix === '648' || threePrefix === '649' || prefix === '65') && (cardNumber.length === 16 || cardNumber.length === 19)) {
+	result = 'Discover';
+}
+
+//checking Maestro
+else if ((fourPrefix === '5018' || fourPrefix === '5020' || fourPrefix === '5038' || fourPrefix === '6304') && (lengthCheck() === true)) {
+	result = 'Maestro';
 }
 
 //case = other network
